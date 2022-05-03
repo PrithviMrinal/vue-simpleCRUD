63f13336e9d68e81ade2fc27ed4b14c0bbd34c6f
SCET <scet@Mac-mini.local>
2022-05-03 12:05:06 +0530

    Empty myCRUD logic

---
+++ src/components/TicTacToe.vue
@@ -0,0 +1,55 @@
+<script>
+import { anyWinner } from "./game-logic";
+
+export default {
+  name: "TicTacToe",
+  components: {},
+  data() {
+    return {
+      board: ["", "", "", "", "", "", "", "", ""],
+      turn: 'X',
+    };
+  },
+  methods: {
+    toggleTurn: function () {
+      if(this.turn==='X'){
+        this.turn='0';
+      }
+      else {
+        this.turn = 'X';
+      }
+    },
+    tick: function (index) {
+      this.board[index] = this.turn;
+      this.toggleTurn();
+    },
+  },
+};
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold text-blue-900 mb-10">TicTacToe Game</h1>
+<!--  <div class="max-w-lg content-center m-auto">
+    <div class="grid grid-cols-3 gap-2 content-start w-fulltext-center p-4">
+      <div
+        v-for="(val, index) in board"
+        :key="index"
+        class="bg-slate-100 drop-shadow-md cursor-pointer align-middle p-8 h-32 rounded-md text-slate-100 font-bold"
+        :class="{
+          'bg-blue-500': val === 'X',
+          'bg-red-500': val === 'O',
+          'hover:drop-shadow-lg': val === '',
+          'cursor-not-allowed': val !== '',
+        }"
+      >
+        {{ index }}
+      </div>
+    </div>
+  </div>-->
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/myCRUD.vue
@@ -0,0 +1,52 @@
+<script>
+//import TicTacToe from "./components/TicTacToe.vue";
+//import TicTacToe_newLogic from "./components/TicTacToe_newLogic.vue"
+
+</script>
+
+<template>
+    <div>
+        <input v-model="prefix" placeholder="Filter prefix">
+    <select size="5" v-model="selected">
+        <option v-for-key="name in filteredNames">{{ name }}</option>
+    </select>
+        <label>Name: <input v-model="first"></label>
+        <label>Surname: <input v-model="last"></label>
+    </div>
+</template>
+
+<!--<template>
+    <body style="text-align:center">
+        <h1>My first simple CRUD operations with vue + vite applications</h1>
+        <form class="main">
+            <div>
+                <label>Search Prefix to Update and or Delete </label>
+                <input v-model="prefix " placeholder="Filter Prefix" alignment="center" v-text="center" text-align="center"/>
+            </div>
+            <div>
+                <select size=10 v-model="selected">
+                    <option v-for-key="name in prefix">{{ name }}</option>
+                </select>
+                <label>First Name </label>
+                <input v-model="First" alignment="center" v-text="center" text-align="center"/>
+            </div>
+            <div>
+            <label>Surname </label>
+                <input v-model="surname " alignment="center" v-text="center" text-align="center"/>
+            </div>
+            <div>
+                <button @click="Create">Create Record</button>
+                <button @click="Update">Update Record</button>
+                <button @click="Delete">Delete Record</button>
+                <button @click="ClearAll" type="reset" value="reset">ClearAll</button>
+            </div>
+        </form>  
+    </body>
+</template>-->
+
+<style scoped>
+ app1{ 
+     color: #42b983;
+ }
+</style>
+
